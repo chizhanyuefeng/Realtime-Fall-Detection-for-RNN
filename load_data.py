cb9318f9cd7ef82ebdd746c34494a6a3f9f87953
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-06-19 14:39:47 +0800

    添加加载数据的类，同时添加获取batch的方法

---
+++ load_data.py
@@ -0,0 +1,31 @@
+import os
+import random
+import numpy as np
+import pandas as pd
+
+class LoadData(object):
+
+    data_file_list = None
+    current_file_index = 0
+    extract_data_size = 0
+
+    def __init__(self, data_path, time_step):
+        if not os.path.exists(data_path):
+            print('%s is not found'%(data_path))
+            raise FileExistsError
+        self.time_step = time_step
+        self.extract_data_size = 50 * self.time_step
+        self.data_file_list = [os.path.join(data_path, file) for file in os.listdir(data_path)]
+
+    def get_next_batch(self, batchsize):
+        data = pd.read_csv(self.data_file_list[self.current_file_index])
+        self.current_file_index += 1
+        data_size = len(data.acc_x.values)
+
+        train_x = []
+        for i in range(batchsize):
+            start = random.randint(1, data_size-self.extract_data_size)
+            train_x.append(data.iloc[start:start+self.extract_data_size, 0:6])
+
+if __name__ == '__main__':
+    data = LoadData('./dataset/train/', 3)
