40add6f1acefeeb07a742ef322cc6818c22c95e3
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-06-21 19:56:13 +0800

    将dynamic_rnn修改为static_rnn，网络得到了大大的改善.后续说明原因

--- build_rnn.py
+++ build_rnn.py
@@ -13,7 +13,6 @@ class AFD_RNN(object):
 
     def build_net_graph(self):
         self.x = tf.placeholder(tf.float32, [None, self.time_step, self.senor_data_num])
-        #self.label = tf.placeholder(tf.float32, [self.class_num])
 
         self._add_input_layer()
         self._add_rnn_layer()
@@ -29,23 +28,25 @@ class AFD_RNN(object):
         self.x_output = tf.reshape(tf.add(tf.matmul(input_x, weights_x), biases_x),
                                    [-1, self.time_step, self.num_units])
 
+
     def _add_rnn_layer(self):
+        self.x_output = tf.unstack(self.x_output, axis=1)
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.num_units)
         self.cell_state = lstm_cell.zero_state(self.batch_size, dtype=tf.float32)
 
         # outputs shape =[batch_size, max_time, cell_state_size]
         # LSTM final_state shape = [2, batch_size, cell_state_size]
-        self.cell_outputs, self.final_state = tf.nn.dynamic_rnn(lstm_cell,
-                                                                self.x_output,
-                                                                initial_state=self.cell_state,
-                                                                time_major=False)
+        self.cell_outputs, self.final_state = tf.nn.static_rnn(lstm_cell,
+                                                               self.x_output,
+                                                               initial_state=self.cell_state)
+
     def _add_output_layer(self):
         outputs = tf.reshape(self.cell_outputs, [-1, self.num_units])
         weights_outputs = self._get_variable_weights([self.num_units, self.class_num], 'outputs_weights')
         biases_outputs = self._get_variable_biases([self.class_num], 'outputs_biases')
 
         return tf.reshape(tf.add(tf.matmul(outputs, weights_outputs), biases_outputs),
-                          [-1, self.time_step, self.class_num])
+                          [self.time_step, self.batch_size, self.class_num])
 
     def _get_variable_weights(self, shape, name):
         return tf.Variable(tf.truncated_normal(shape, stddev=0.1), dtype=tf.float32, name=name)
