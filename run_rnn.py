a418d779784467c266184aa04d609c1c0aa21ffa
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-06-23 18:36:50 +0800

    将绘图方法添加到类中

--- run_rnn.py
+++ run_rnn.py
@@ -40,58 +40,56 @@ class Run_AFD_RNN(object):
     def run_stop(self):
         self._sess.close()
 
-def update_show_data(data, step, update_data):
-    for i in range(step):
-        data.pop(0)
-        data.append(update_data[i])
+    def _update_show_data(self, data, step, update_data):
+        for i in range(step):
+            data.pop(0)
+            data.append(update_data[i])
+
+    def draw_flow(self, test_data, test_label):
+        data_size = test_data.shape[0]
+        plt.axis([0, 151, -20, 20])
+        plt.ion()
+
+        x = [_ for _ in range(150)]
+        ax = [0 for _ in range(150)]
+        ay = [0 for _ in range(150)]
+        az = [0 for _ in range(150)]
+
+        num = int(data_size / 5)
+
+        start_time = time.time()
+
+        for i in range(num):
+            if i > int(time_step/5):
+                predict = run.run(test_data[i * 5 - time_step: i * 5, :])
+                title = 'correct:' + Label[test_label[i * 5]] + '     predict:' + Label[predict[int(time_step - 1)][0]]
+            else:
+                title = 'correct:' + Label[test_label[i * 5]] + '     predict:' + 'unknow'
+
+            self._update_show_data(ax, 5, test_data[i * 5:i * 5 + 5, 0])
+            self._update_show_data(ay, 5, test_data[i * 5:i * 5 + 5, 1])
+            self._update_show_data(az, 5, test_data[i * 5:i * 5 + 5, 2])
+
+            plt.cla()
+            plt.plot(x, ax)
+            plt.plot(x, ay)
+            plt.plot(x, az)
+
+            plt.title(title)
+            plt.draw()
+            plt.pause(0.000001)
+        during = str(time.time() - start_time)
+        print('检测耗时=', during)
 
 if __name__ == '__main__':
     net_config = parser_cfg_file('./config/rnn_net.cfg')
-    time_step = 40
+    time_step = 80
     class_num = int(net_config['class_num'])
 
     run = Run_AFD_RNN('./model/', time_step=time_step)
     data_load = DataLoad('./dataset/test/', time_step=time_step, class_num=class_num)
-    # x, y = data_load.get_batch(1, [2])
-
-    # print(x)
-    # run = Run_AFD_RNN('./model/', time_step=time_step)
-    # predict = run.run(x)
-    # print(predict)
-    # print(y)
 
     test_data, test_label = data_load.get_test_data()
-    data_size = test_data.shape[0]
-    plt.axis([0, 151, -20, 20])
-    plt.ion()
+    run.draw_flow(test_data, test_label)
 
-    x = [_ for _ in range(150)]
-    ax = [0 for _ in range(150)]
-    ay = [0 for _ in range(150)]
-    az = [0 for _ in range(150)]
-
-    num = int(data_size / 5)
-
-    start_time = time.time()
-
-    for i in range(num):
-        if i >10 :
-            predict = run.run(test_data[i*5-time_step: i*5, :])
-            title = 'correct:' + Label[test_label[i * 5]] + '     predict:' + Label[predict[int(time_step-1)][0]]
-        else:
-            title = 'correct:' + Label[test_label[i * 5]] + '     predict:' + 'unknow'
-
-        update_show_data(ax, 5, test_data[i*5:i*5+5,0])
-        update_show_data(ay, 5, test_data[i * 5:i * 5 + 5, 1])
-        update_show_data(az, 5, test_data[i * 5:i * 5 + 5, 2])
-
-        plt.cla()
-        plt.plot(x, ax)
-        plt.plot(x, ay)
-        plt.plot(x, az)
-
-        plt.title(title)
-        plt.draw()
-        plt.pause(0.000001)
-    during = str(time.time() - start_time)
-    print('检测耗时=', during)
+    run.run_stop()
