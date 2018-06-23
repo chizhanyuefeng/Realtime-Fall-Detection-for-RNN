9ce022492a941b67d6f3a1b7121ef7a770213dea
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-06-23 14:09:17 +0800

    将run_rnn做成类，方便测试使用

--- run_rnn.py
+++ run_rnn.py
@@ -1,29 +1,50 @@
 # -*- coding:utf-8 -*-
+import time
+import numpy as np
 import tensorflow as tf
+from matplotlib import pyplot as plt
 from build_rnn import AFD_RNN
 from data_load import DataLoad
 from utils import parser_cfg_file
 
-def run_net(mode_dir, data):
 
-    ckpt = tf.train.get_checkpoint_state(mode_dir)
-    if ckpt is None:
-        raise FileExistsError(str(mode_dir, '没有模型可以加载'))
 
-    batch_size = data.shape[0]
-    net_config = parser_cfg_file('./config/rnn_net.cfg')
-    rnn_net = AFD_RNN(net_config, batch_size)
-    predict = rnn_net.build_net_graph()
-    predict_tensor = tf.argmax(predict, axis=2)
-    saver = tf.train.Saver()
+Label = {1: 'STD', 2: 'WAL', 3: 'JOG', 4: 'JUM', 5: 'STU', 6: 'STN', 7: 'SCH', 8: 'SIT', 9: 'CHU',
+         10: 'LYI', 0: 'FALL', 15: 'CSI', 16: 'CSO'}
 
-    with tf.Session() as sess:
+class Run_AFD_RNN(object):
+
+    def __init__(self, mode_dir):
+        ckpt = tf.train.get_checkpoint_state(mode_dir)
+        if ckpt is None:
+            raise FileExistsError(str(mode_dir, '没有模型可以加载'))
+
+        batch_size = data.shape[0]
+        net_config = parser_cfg_file('./config/rnn_net.cfg')
+        self.rnn_net = AFD_RNN(net_config, batch_size)
+        predict = rnn_net.build_net_graph()
+        self.predict_tensor = tf.argmax(predict, axis=2)
+        saver = tf.train.Saver()
+        self.sess = tf.Session()
         # 加载参数
-        saver.restore(sess, ckpt.model_checkpoint_path)
+        saver.restore(self.sess, ckpt.model_checkpoint_path)
 
-        predict = sess.run(predict_tensor, feed_dict={rnn_net.input_tensor: data})
 
-        print(predict)
+    def run_net(self, data):
+
+        predict = sess.run(self.predict_tensor, feed_dict={self.rnn_net.input_tensor: data})
+        return predict
+
+    def run_stop(self):
+        self.sess.close()
+
+def update_show_data(data, step, update_data):
+    for i in range(step):
+        data.pop(0)
+        data.append(update_data[i])
+
+
+
 
 if __name__ == '__main__':
     net_config = parser_cfg_file('./config/rnn_net.cfg')
@@ -31,6 +52,39 @@ if __name__ == '__main__':
     class_num = int(net_config['class_num'])
 
     data_load = DataLoad('./dataset/test/', time_step, class_num)
-    x, y = data_load.get_batch(1)
-    print(y.argmax(2))
-    run_net('./model/', x)
+    # x, y = data_load.get_batch(1)
+    # print(y.argmax(2))
+    # run_net('./model/', x)
+
+
+    test_data, test_label = data_load.get_test_data()
+    data_size = test_data.shape[0]
+    plt.axis([0, 151, -20, 20])
+    plt.ion()
+
+
+    x = [_ for _ in range(150)]
+    ax = [0 for _ in range(150)]
+    ay = [0 for _ in range(150)]
+    az = [0 for _ in range(150)]
+
+    num = int(data_size / 5)
+    print(data_size)
+
+    start_time = time.time()
+
+    for i in range(num):
+
+        update_show_data(ax, 5, test_data[i*5:i*5+5,0])
+        update_show_data(ay, 5, test_data[i * 5:i * 5 + 5, 1])
+        update_show_data(az, 5, test_data[i * 5:i * 5 + 5, 2])
+
+        plt.cla()
+        plt.plot(x, ax)
+        plt.plot(x, ay)
+        plt.plot(x, az)
+        plt.title(Label[test_label[i*5]])
+        plt.draw()
+        plt.pause(0.000001)
+    during = str(time.time() - start_time)
+    print('检测耗时=', during)
